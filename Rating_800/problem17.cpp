988691826be9e4980cdc31638d8af6dcc2897449
Harsh <harshtyagi0008@icloud.com>
2026-06-03 21:39:46 +0530

    add problem 17

---
+++ Rating_800/problem17.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n,k,x;
+    cin>>n>>k>>x;
+    if(x!=1){
+        cout<<"YES"<<endl;
+        cout<<n<<endl;
+        for(int i=1;i<=n;i++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        if(k==1 || (k==2 && n%2==1)){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+            if(n%2==0){
+                cout<<n/2<<endl;
+                for(int i=1;i<=n/2;i++){
+                    cout<<2<<" ";
+                }
+                cout<<endl;
+            }
+            else{
+                cout<<(n-3)/2+1<<endl;
+                for(int i=1;i<=(n-3)/2;i++){
+                    cout<<2<<" ";
+                }
+                cout<<3<<endl;
+            }
+        }
+    }
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
