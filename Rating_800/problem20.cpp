4237ecc765afc76f2ea5e36bb885da0b54467f19
Harsh <harshtyagi0008@icloud.com>
2026-06-04 20:03:50 +0530

    add problem 20

---
+++ Rating_800/problem20.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<n+1-a[i]<<" ";
+    }
+    cout<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
