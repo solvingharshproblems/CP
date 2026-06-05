9405320a0eccae54ed90fb89753d53e9747da666
Harsh <harshtyagi0008@icloud.com>
2026-06-05 10:31:09 +0530

    add problem 21

---
+++ Rating_800/problem21.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int res=0,count=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        res=max(res,count);
+    }
+    cout<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
