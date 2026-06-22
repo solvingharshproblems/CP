79383bbba54c059a2bbcb445d9ee7eb148a30450
Harsh <harshtyagi0008@icloud.com>
2026-06-22 10:01:53 +0530

    add problem 25

---
+++ Rating_900/problem25.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    long long n,m,i,j;
+    cin>>n>>m>>i>>j;
+    cout<<1<<" "<<1<<" "<<n<<" "<<m<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
