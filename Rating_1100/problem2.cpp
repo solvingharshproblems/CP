e3a58b612f1072e985d9033f93a2a7e0b9757c77
Harsh <harshtyagi0008@icloud.com>
2026-07-09 14:50:59 +0530

    add problem 2

---
+++ Rating_1100/problem2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> a(n),b(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>b[i];
+    }
+    int Max=0,sum=0,res=0;
+    for(int i=0;i<min(n,k);i++){
+        sum+=a[i];
+        Max=max(Max,b[i]);
+        res=max(res,sum+(k-(i+1))*Max);
+    }
+    cout<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
