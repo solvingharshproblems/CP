540dd548385a52ee255af21dddc361367622cdf2
Harsh <harshtyagi0008@icloud.com>
2026-07-25 12:19:36 +0530

    add problem 2

---
+++ Rating_1200/problem2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n;
+    cin>>n;
+    vector<long long> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    long long res=0;
+    for(int i=1;i<=58;i++){
+        set<long long> s;
+        long long k=1LL<<i;
+        for(int j=0;j<n;j++){
+            s.insert(a[j]%k);
+        }
+        if(s.size()==2){
+            res=k;
+            break;
+        }
+    }
+    cout<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
