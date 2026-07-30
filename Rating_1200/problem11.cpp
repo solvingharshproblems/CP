16d0a6b2e9ec3d6acaa6bb7fbb68da991c45d30d
Harsh <harshtyagi0008@icloud.com>
2026-07-30 18:44:42 +0530

    add problem 11

---
+++ Rating_1200/problem11.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    long long res=0;
+    vector<bool> a(n,false);
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j+=i){
+            if(s[j-1]=='1'){
+                break;
+            }
+            if(!a[j]){
+                res+=i;
+                a[j]=true;
+            }
+        }
+    }
+    cout<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
