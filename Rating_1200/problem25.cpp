3eefe679b091413188a7b02361d35c8b2bd586ba
Harsh <harshtyagi0008@icloud.com>
2026-08-06 10:58:57 +0530

    add problem 25

---
+++ Rating_1200/problem25.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    unordered_map<int,int> mp;
+    long long res=0;
+    for(int i=0;i<n;i++){
+        int key=a[i]-(i+1);
+        res+=mp[key];
+        mp[key]++;
+    }
+    cout<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
