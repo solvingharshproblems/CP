d45c19fc5a751621b89326e6e5182a1ded60ba2e
Harsh <harshtyagi0008@icloud.com>
2026-08-11 09:30:41 +0530

    add problem 5

---
+++ Rating_1300/problem5.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD=1e9+7;
+void solve(){
+    string s;
+	cin>>s; 
+	int n=s.size(); 
+	long long int res=1; 
+	int len=1,curr=1;
+	for(int i=1;i<n;i++){
+		if(s[i]!=s[i-1]){
+			len++;
+			res=(res*curr)%MOD; 
+			curr=1; 
+		} 
+        else{
+			curr++; 
+		}
+	}
+	res=(res*curr)%MOD; 
+	for(int i=1;i<=n-len;i++){ 
+		res=(res*i)%MOD;
+	}
+    cout<<n-len<<" "<<res<<endl;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+}
