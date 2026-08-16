a7acb19eb5bf375c76e6a3bab0dac452fb10d00d
Harsh <harshtyagi0008@icloud.com>
2026-08-16 11:19:31 +0530

    fixed code

--- Rating_1300/problem15.cpp
+++ Rating_1300/problem15.cpp
@@ -3,28 +3,28 @@ using namespace std;
 const int MOD=1e9+7;
 void solve() {
     int n;
-    cin >> n;
-    vector<pair<int, int>> a;
-    for (int i = 0; i < n; i++) {
+    cin>>n;
+    vector<pair<int,int>> a;
+    for(int i=0;i<n;i++){
         vector<int> v;
         int m;
-        cin >> m;
-        for (int j = 0; j < m; j++) {
+        cin>>m;
+        for(int j=0;j<m;j++){
             int x;
-            cin >> x;
-            v.push_back(x - j + 1);
+            cin>>x;
+            v.push_back(x-j+1);
         }
-        sort(v.begin(), v.end());
-        a.push_back({v[m - 1], m});
+        sort(v.begin(),v.end());
+        a.push_back({v[m-1],m});
     }
-    sort(a.begin(), a.end());
-    int ans = a[0].first;
-    int increase = a[0].second;
-    for (int i = 1; i < n; i++) {
-        ans = max(ans, a[i].first - increase);
-        increase += a[i].second;
+    sort(a.begin(),a.end());
+    int ans=a[0].first;
+    int increase=a[0].second;
+    for(int i=1;i<n;i++){
+        ans=max(ans,a[i].first-increase);
+        increase+=a[i].second;
     }
-    cout << ans << endl;
+    cout<<ans<<endl;
 }
 int main(void){
     ios_base::sync_with_stdio(false);
