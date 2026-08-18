481c561258e5b8c6519a2cb58853e8d19b9f5fd5
Harsh <harshtyagi0008@icloud.com>
2026-08-18 09:16:17 +0530

    fixed problem 5's solution

--- Rating_1300/problem5.cpp
+++ Rating_1300/problem5.cpp
@@ -1,27 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MOD=1e9+7;
+const int MOD=998244353;
 void solve(){
     string s;
 	cin>>s; 
 	int n=s.size(); 
-	long long int res=1; 
-	int len=1,curr=1;
-	for(int i=1;i<n;i++){
+	long long res=1; 
+	int len=1,curr=1; 
+	for(int i=1;i<n;i++){ 
 		if(s[i]!=s[i-1]){
-			len++;
+			len++; 
 			res=(res*curr)%MOD; 
 			curr=1; 
 		} 
-        else{
+		else{
 			curr++; 
 		}
 	}
-	res=(res*curr)%MOD; 
+	res=(res*curr)%MOD;
 	for(int i=1;i<=n-len;i++){ 
 		res=(res*i)%MOD;
 	}
-    cout<<n-len<<" "<<res<<endl;
+	cout<<n-len<<" "<<res<<endl;
 }
 int main(void){
     ios_base::sync_with_stdio(false);
