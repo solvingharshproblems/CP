117e79556f8286deb646f650a3172ddc11c668c1
Harsh <harshtyagi0008@icloud.com>
2026-09-20 10:39:15 +0530

    add problem 15

---
+++ Rating_1700/problem15.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int cnt[1000001];
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int tc;
+  cin >> tc;
+  while (tc--) {
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      cnt[a[i]]++;
+    }
+
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+
+      ans += ll(cnt[a[i]] - 1) * (cnt[a[i]] - 2);
+
+      for (int b = 2; (ll)a[i] * b * b <= 1000000ll; b++) {
+        ans += ll(cnt[a[i] * b]) * cnt[a[i] * b * b];
+      }
+    }
+
+    for (int i = 0; i < n; i++)
+      cnt[a[i]]--;
+
+    cout << ans << "\n";
+  }
+
+  return 0;
+}
